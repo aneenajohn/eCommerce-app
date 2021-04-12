76cdc92136f8b2590be919fb9b0679d3a2f739f1
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-04-12 18:45:39 +0530

    Updated initial setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//      <h1>Hello</h1>
+      
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import "./styles.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-     <h1>Hello</h1>
-      
-      </header>
+      <h1 className="app-header">tanaypratap's box</h1>
+      <div className="app-body">Put your app body here</div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
 
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
+  rootElement
 );
-
-
